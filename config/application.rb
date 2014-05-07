4b3e011814d9070b6539ad0b751003f31c12170e
Billie Zhang <billiecoder@gmail.com>
2014-05-07 21:15:35 +0800

    create user model

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ end
 
 module Ppweb
   class Application < Rails::Application
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
