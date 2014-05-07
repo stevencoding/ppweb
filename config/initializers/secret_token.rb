5ba3b50d730225bda7407b2ec64afbff47fa299e
Billie Zhang <billiecoder@gmail.com>
2014-05-07 17:19:57 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppweb::Application.config.secret_token = '89f396312dada5a64f6407c66de3d9986556e72e437aaef35a10fba222d7fb028b92b7d7843d5b126e37a650f738babf88a35ead98ad8464ae48f2ef3d6d946d'
