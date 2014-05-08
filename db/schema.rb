3ae846748c98b537604a2c410150525b4aa0a223
Billie Zhang <billiecoder@gmail.com>
2014-05-08 17:52:39 +0800

    add role column for users table #3

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140507131330) do
+ActiveRecord::Schema.define(:version => 20140508092221) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.string   "token"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "role",            :default => "student"
   end
 
 end
