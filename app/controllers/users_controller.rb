c09a9891ace4e53456c26beeecdb1a45ce2b3728
Billie Zhang <billiecoder@gmail.com>
2014-05-08 08:52:48 +0800

    signup is done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
+#encoding: utf-8
 class UsersController < ApplicationController
   def welcome
   end
 
   def signup
+    @user = User.new
   end
 
   def login
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      cookies.permanent[:token] = @user.token
+      redirect_to :root, :notice => "注册成功"
+    else
+      render :signup
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest, :token
+  has_secure_password
+  attr_accessible :email, :name, :password, :password_confirmation, :token
+  before_create { generate_token(:token) }
+
+  validates :name, :presence => true, :uniqueness => {:case_sensitive => false}
+  validates :email, :presence => true, :uniqueness => {:case_sensitive => false}, :email_format => true
+  validates :password, :length => { :minimum => 6 }, :on => :create
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
