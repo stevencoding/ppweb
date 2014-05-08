83564bc62d1172332982adf026800c6fb7f2ab19
Peter Wang <happypeter1983@gmail.com>
2014-05-08 20:58:39 +0800

    better profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,20 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, :uniqueness => {:case_sensitive => false}, :email_format => true
   validates :password, :length => { :minimum => 6 }, :on => :create
 
+
+  def has_avatar?
+    self.read_attribute(:avatar).present?
+  end
+
+  def gravatar_url
+    gravatar_id = Digest::MD5.hexdigest(self.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=512&d=retro"
+  end
+
+  def final_avatar_url
+    self.has_avatar? ? self.avatar_url : self.gravatar_url
+  end
+  
   def generate_token(column)
     begin
       self[column] = SecureRandom.urlsafe_base64
---
+++ db/migrate/20140508130407_add_avater_to_users.rb
@@ -0,0 +1,5 @@
+class AddAvaterToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :avatar, :string
+  end
+end
