a767bac852739ad4538c9e40c9f5a46370028b9d
Billie Zhang <billiecoder@gmail.com>
2014-05-15 14:43:16 +0800

    mark notifications as read

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,12 @@
 class NotificationsController < ApplicationController
+  after_filter :mark_read
+
   def index
     @notifications = current_user.notifications.recent
+  end
+
+  private
+  def mark_read
+    current_user.notifications.update_all(unread: false)
   end
 end
