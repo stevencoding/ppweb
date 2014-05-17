e4af318f5ee53e5117242d4c20593558b326ea03
Billie Zhang <billiecoder@gmail.com>
2014-05-17 10:29:38 +0800

    limit user permissions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def owner?(item)
+    return false if item.blank? || current_user.blank?
+    item.user_id == current_user.id
+  end
+
+  def edit?(user)
+    return false if current_user.blank?
+    current_user.id == user.id
+  end
 end
