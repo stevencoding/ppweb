ec98291803e669b4523b808d79aa66ccbbd6e4db
Billie Zhang <billiecoder@gmail.com>
2014-05-25 10:38:35 +0800

    only show published events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :set_return_to, only: [:index]
 
   def index
-    @events = Event.all.reverse
+    @events = Event.where(published: true).reverse
     @attended_events = current_user.attended_events.compact.sort_by(&:start) if current_user
   end
 end
