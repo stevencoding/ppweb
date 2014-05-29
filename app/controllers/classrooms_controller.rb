a3d192e5dcaf1901fd230cdc77d82a4f5a91fdce
Liu Jiyang <2411mail@gmail.com>
2014-05-29 19:16:46 +0800

    show username when video chat

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,8 +1,10 @@
 class ClassroomsController < ApplicationController
   layout 'no_header_footer'
   def show
+    @username = current_user.username
   end
   def pproom
+    @username = current_user.username
     # use username for identical roomname
     @roomname = params[:roomname]
     if !User.find_by_username(@roomname)
