d3f2550bb21b2cfd7dfba88b6af795862b0d5358
Liu Jiyang <2411mail@gmail.com>
2014-06-25 13:56:05 +0800

    PAY BY BEAN | STEP 6: pay teacher

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,14 @@ class OrdersController < ApplicationController
 
   def checkout
     @event = Event.find_by_uid(params[:uid])
+
     current_user.bean = current_user.bean - @event.price.to_i * 10
     current_user.save
+
+    owner = User.find(@event.user_id)
+    owner.bean = owner.bean + @event.price.to_i * 10
+    owner.save
+
     if @event.add_member(current_user)
       redirect_to event_path(uid: params[:uid]), notice: t("event.flashes.successfully_joined")
     else
