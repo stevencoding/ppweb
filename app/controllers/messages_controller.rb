300e4d142a335cd6a71d411cfba274cc9390e88d
Billie Zhang <billiecoder@gmail.com>
2014-06-30 23:07:08 +0800

    notify message receiver and i18n

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,8 @@ class MessagesController < ApplicationController
   def create
     @message = Message.create(params[:message])
     @message.save
+    Notification.notify("message", @message.sender, @message.receiver, @message)
 
-    redirect_to_target_or_default :root, notice: "successfully sent message"
+    redirect_to_target_or_default :root, notice: t("message.suc_sent")
   end
 end
