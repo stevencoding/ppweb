a0de89c088541277674732ea47ce4266954af767
Billie Zhang <billiecoder@gmail.com>
2014-07-03 11:47:54 +0800

    not show expired events on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :set_return_to, only: [:index]
 
   def index
-    @events = Event.where(published: true).reverse
+    @events = Event.where("published = true AND start >= :current_time", {current_time: Time.now.utc}).reverse
     @attended_events = current_user.attended_events.compact.sort_by(&:start) if current_user
   end
 end
