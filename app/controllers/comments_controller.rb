2633cae81f1a48af10d4d83514d7de1561d835b8
Peter Wang <happypeter1983@gmail.com>
2014-07-17 15:56:31 +0800

    ajaxify comments  fix #163

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,5 @@ class CommentsController < ApplicationController
 	def create
 		@comment = current_user.comments.build(params[:comment])
     @comment.save
-    redirect_to_target_or_default root_url
 	end
 end
