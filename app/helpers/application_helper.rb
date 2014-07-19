5bd10f99489c3d9ea5b4b7bace7f78be277d1fce
Peter Wang <happypeter1983@gmail.com>
2014-07-19 12:40:21 +0800

    add markdown support #170

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
     return false if current_user.blank?
     current_user.id == user.id
   end
+
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
 end
