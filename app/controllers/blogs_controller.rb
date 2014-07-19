4ce687d5d639f29129bb1af58066e40cfca69f2b
Peter Wang <happypeter1983@gmail.com>
2014-07-19 17:05:16 +0800

    markdown style agin #170

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,6 +18,10 @@ class BlogsController < ApplicationController
   def update
     blog = Blog.find(params[:id])
     blog.update_attributes(params[:blog])
+    title = params[:blog][:title]
+    name = PinYin.of_string(title).join('-').downcase
+    blog.name = name
+    blog.save!
     redirect_to blog_path(blog), :notice => "blog updated"
   end
   def create
