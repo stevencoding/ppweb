183af451dabb5d0bd7c653eabb2deb8ea33397d4
Billie Zhang <billiecoder@gmail.com>
2014-07-21 18:19:41 +0800

    comment issue #172

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,7 @@
 class Issue < ActiveRecord::Base
   attr_accessible :content, :title, :user_id
-  belongs_to :user
   scope :recent, -> { order('id DESC') }
+
+  belongs_to :user
+  has_many :comments, as: 'commentable'
 end
